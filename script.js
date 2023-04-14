2825373ec4ab9b1905be0e24fa13c25f43139ed6
WhidanB <danyphone95@gmail.com>
2023-04-14 16:40:17 +0200

    score ok 	modified:   script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 /*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🙌 Correct Number!';
@@ -9,37 +9,46 @@ document.querySelector('.guess').value=23;
 console.log(document.querySelector('.guess').value);
 */
 
-
-const secretNumber = Math.trunc(Math.random()*20)+1;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 let highscore = 0;
 
-document.querySelector('.number').textContent = secretNumber;
-document.querySelector('.score').textContent = score;
-document.querySelector('.highscore').textContent = highscore;
-document.querySelector('.check').addEventListener('click', ()=>{
-    const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
-    
+document.querySelector(".number").textContent = secretNumber;
+document.querySelector(".score").textContent = score;
+document.querySelector(".highscore").textContent = highscore;
+document.querySelector(".check").addEventListener("click", () => {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
 
-    if(!guess){
-        document.querySelector('.message').textContent = '😅 No number!';
-    }else if(guess===secretNumber){
-        document.querySelector('.message').textContent = '🙌 Well done !';
-        ++score;
-        ++highscore;
-        console.log(score);
-        document.querySelector('.score').textContent = score;
-        document.querySelector('.highscore').textContent = highscore;
-    }else if(guess>secretNumber){
-        document.querySelector('.message').textContent = '⬇️ Too high !';
-        --score;
-        document.querySelector('.score').textContent = score;
-    }else if(guess<secretNumber){
-        document.querySelector('.message').textContent = '⬆️ Too low !';
-        --score;
-        document.querySelector('.score').textContent = score;
+  if (!guess) {
+    document.querySelector(".message").textContent = "😅 No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🙌 Well done !";
+    ++score;
+    ++highscore;
+    console.log(score);
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".highscore").textContent = highscore;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "⬇️ Too high !";
+      --score;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "❌ YOU LOST !";
+      score--;
+      document.querySelector(".score").textContent = score;
     }
-    
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "⬆️ Too low !";
+      --score;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "❌ YOU LOST !";
+      score--;
+      document.querySelector(".score").textContent = score;
+    }
+  }
 });
